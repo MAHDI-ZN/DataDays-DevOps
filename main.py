dc77bf254c0a1bcc0053df9463f8a2dedcd19f73
M.MOHAMMADI <mahdimohammadizengan@gmail.com>
2022-08-17 01:10:18 +0430

    Docker image of api completed and api container connected to redis container successfully

--- main.py
+++ main.py
@@ -1,48 +1,52 @@
-from fastapi import FastAPI , Path
+import pickle
+import redis
+from fastapi import FastAPI, Path
 from pydantic import BaseModel
+redis_client = redis.Redis(host='myred', port=6379, db=0)
+
 
 app = FastAPI()
 
+
 class Person(BaseModel):
-    name : str
-    lastName : str
+    name: str
+    lastName: str
 
-DefaultNameSet = {
-    1:{
-        "name" : "John",
-        "lastName" : "Wick"
-    },
-    2:{
-        "name" : "John",
-        "lastName" : "Snow"
-    },
-    3:{
-        "name" : "Walter",
-        "lastName" : "White"
-    }
-}
 
-@app.get("/ExtractPerson/{Id}")
-def Extract_Full_Name_Via_Id(Id: int = Path(None,description="Enter the Id of person you want his name:",ge=1,le=10)):
-    return DefaultNameSet[Id]
+DefaultNameSet = pickle.loads(redis_client.get('dictionary'))
 
-@app.post("/CreatePerson/{Id}")
-def Create_Person( item: Person,Id: int = Path(None,description="Enter the Id you want to associate with your person:",ge=1,le=10)):
+print(DefaultNameSet[1])
+@app.get("/ExtractPerson/{id}")
+def extract_full_name_via_id(
+        id: int = Path(None, description="Enter the id of person you want his name:", ge=1, le=10)):
+    return DefaultNameSet[id]
+
+
+@app.post("/CreatePerson/{id}")
+def create_person(item: Person,
+                  id: int =
+                  Path(None, description="Enter the id you want to associate with your person:", ge=1, le=10)):
     if id in DefaultNameSet:
-        return {"Error" : "Person Id already exists"}
-    DefaultNameSet[Id] = item
+        return {"Error": "Person id already exists"}
+    DefaultNameSet[id] = item
+    redis_client.set('dictionary', pickle.dumps(DefaultNameSet))
     return {"Created": item}
 
-@app.put("/UpdatePerson/{Id}")
-def Update_Person( item: Person,Id: int = Path(None,description="Enter the Id of the person you want to update:",ge=1,le=10)):
-    if Id not in DefaultNameSet:
-        return {"Error" : "Person Id doesn't exist"}
-    DefaultNameSet[Id] = item
+
+@app.put("/UpdatePerson/{id}")
+def update_person(item: Person,
+                  id: int = Path(None, description="Enter the id of the person you want to update:", ge=1, le=10)):
+    if id not in DefaultNameSet:
+        return {"Error": "Person id doesn't exist"}
+    DefaultNameSet[id] = item
+    redis_client.set('dictionary', pickle.dumps(DefaultNameSet))
     return {"Update": item}
 
-@app.delete("/DeletePerson/{Id}")
-def Delete_Person(Id: int = Path(None,description="Enter the Id of the person you want to delete:",ge=1,le=10)):
-    if Id not in DefaultNameSet:
-        return {"Error" : "Person Id doesn't exist"}
-    DefaultNameSet[Id] = None;
-    return {"Deleted": Id}
+
+@app.delete("/DeletePerson/{id}")
+def delete_person(id: int = Path(None, description="Enter the id of the person you want to delete:", ge=1, le=10)):
+    if id not in DefaultNameSet:
+        return {"Error": "Person id doesn't exist"}
+    del DefaultNameSet[id]
+    redis_client.set('dictionary', pickle.dumps(DefaultNameSet))
+    return {"Deleted": id}
